548f79df663d68249ae552703c4636bfd0a5fc3a
daffakurnia11 <72376750+daffakurnia11@users.noreply.github.com>
2022-03-04 18:40:08 +0700

    Creating Custom View
    Creating MyButton and MyEditText custom views classes
    
    Dicoding Materials :
    Belajar Fundamental Aplikasi Android - Latihan Membuat Custom Button dan EditText

--- app/src/main/java/me/daffakurnia/android/firstapplication/MainActivity.kt
+++ app/src/main/java/me/daffakurnia/android/firstapplication/MainActivity.kt
@@ -3,8 +3,13 @@ package me.daffakurnia.android.firstapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Button
+import android.widget.Toast
 import me.daffakurnia.android.firstapplication.barvolume.BarVolumeActivity
+import me.daffakurnia.android.firstapplication.customview.MyButton
+import me.daffakurnia.android.firstapplication.customview.MyEditText
 import me.daffakurnia.android.firstapplication.fragment.FragmentActivity
 import me.daffakurnia.android.firstapplication.intent.IntentActivity
 import me.daffakurnia.android.firstapplication.layouts.ConstraintLayoutActivity
@@ -12,7 +17,8 @@ import me.daffakurnia.android.firstapplication.layouts.LayoutsActivity
 
 class MainActivity : AppCompatActivity() {
 
-
+    private lateinit var myButton: MyButton
+    private lateinit var myEditText: MyEditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,5 +53,24 @@ class MainActivity : AppCompatActivity() {
         btnConstraintLayouts.setOnClickListener {
             startActivity(Intent(this@MainActivity, ConstraintLayoutActivity::class.java))
         }
+
+        myButton = findViewById(R.id.my_button)
+        myEditText = findViewById(R.id.my_edit_text)
+        setMyButtonEnable()
+        myEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+            }
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                setMyButtonEnable()
+            }
+            override fun afterTextChanged(s: Editable) {
+            }
+        })
+        myButton.setOnClickListener { Toast.makeText(this@MainActivity, myEditText.text, Toast.LENGTH_SHORT).show() }
+    }
+
+    private fun setMyButtonEnable() {
+        val result = myEditText.text
+        myButton.isEnabled = result != null && result.toString().isNotEmpty()
     }
 }
